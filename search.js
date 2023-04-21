ee2ab79f28e5f471499ce099380ca0d44aed7dcb
Jason Tran <42780124+JasonT1085@users.noreply.github.com>
2023-04-20 20:07:12 -0700

    added animations and fixed tile positions on live search

--- search.js
+++ search.js
@@ -1,16 +1,28 @@
 const searchInput = document.getElementById('search-bar');
+const championGrid = document.querySelector('.champion-grid');
 
 searchInput.addEventListener('input', function() {
   const searchTerm = this.value.trim().toLowerCase();
   const championTiles = document.querySelectorAll('.champion-tile');
+  const tilesToShow = [];
 
   championTiles.forEach(championTile => {
     const championName = championTile.getAttribute('data-champion-name');
 
     if (championName.includes(searchTerm)) {
-      championTile.classList.remove('hidden');
+      tilesToShow.push(championTile);
     } else {
       championTile.classList.add('hidden');
     }
   });
+
+  tilesToShow.forEach((championTile, index) => {
+    championTile.classList.remove('hidden');
+    const row = Math.floor(index / 7);
+    const col = index % 7;
+    championTile.style.gridRow = row + 1;
+    championTile.style.gridColumn = col + 1;
+  });
+
+  championGrid.style.gridTemplateRows = `repeat(${Math.ceil(tilesToShow.length / 7)}, 1fr)`;
 });
